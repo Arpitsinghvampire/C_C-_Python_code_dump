680d67b8a095b6b4b8a9326028804fbfef6cd9da
ARPIT SINGH <112745019+Arpitsinghvampire@users.noreply.github.com>
2025-12-13 11:35:19 +0530

    Add files via upload

---
+++ beginner1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"ENTER THE FIRST NUMBER";
+    int number1 ;
+    std::cin>>number1;
+
+    std::cout<<"ENTER THE SECOND NUMBER";
+    int number2;
+    std::cin>>number2;
+
+    std::cout<<"THE FINAL SUM IS "<< number1+number2;
+}
