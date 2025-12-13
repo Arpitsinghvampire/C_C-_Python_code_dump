1cd9383d110b73706690b37b7c721969b0f0bdaa
ARPIT SINGH <112745019+Arpitsinghvampire@users.noreply.github.com>
2025-12-13 11:35:47 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 10 
+
+/* HERE WE WRITE THE FUNCTION PROTOTYPE */
+int* bubbleSort(int *array1 , int size);
+
+int main()
+{
+ //take the input of the array 
+ int size ;
+ printf("ENTER THE SIZE OF THE ARRAY YOU WANT");
+ scanf("%d" , &size);
+  
+  int *array =(int*)malloc(sizeof(int)*size);
+
+ for(int i=0; i<size;i++)
+ {
+    printf("Enter the number ");
+    scanf("%d" , array+i); //this stores the address also 
+
+ }
+
+  for(int i=0; i<size;i++)
+ {
+    printf("%d\n" , array[i]); //this stores the address also 
+
+ }
+
+
+ //now lets do the bubble sort algorithm and then return the  sorted array 
+ printf("THE SORTED ARRAY IS ");
+ int* sorted_array= bubbleSort(array , size);
+ for(int j=0;j<size;j++)
+{
+    printf("%d\n" , sorted_array[j]);
+}
+}
+int* bubbleSort(int* array1 , int size)
+ {
+  for(int j=0;j<=size-1;j++)
+ {
+    for(int i=0; i<size-1;i++)
+    {
+    //swap the elements when the element is greater than the successor 
+    if(array1[i]>array1[i+1])
+    {
+     int temp = array1[i+1];
+     array1[i+1] = array1[i];
+     array1[i] = temp;
+    }
+    }
+ }
+  return array1;
+}
