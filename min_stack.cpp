e5478df16971ca1bd246511ad8dd5d85e51f043c
ARPIT SINGH <112745019+Arpitsinghvampire@users.noreply.github.com>
2026-06-21 22:53:17 +0530

    Add files via upload

---
+++ min_stack.cpp
@@ -0,0 +1,44 @@
+//min_stack.cpp
+
+//here in this question we have to get the minimum of the stack 
+
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	stack<int> s1;
+
+	cout<<"ENTER THE NUMBERS YOU WANT TO CHECK AND FIND THE MIN OF  ,PRESS -1 TO STOP TAKING THE INPUT "<<endl;
+
+	while(true)
+	{
+		int number ;
+		cin>> number ;
+		if(number == -1)
+		{
+			break;
+		}
+		else
+		{
+			if(s1.empty())
+			{
+				s1.push(number);
+			}
+			else
+			{
+				//we compare the top of the stack with the number if the number is less , we pop the top and then add the element 
+				if(number < s1.top())
+				{
+					//pop the element , and then push the min element 
+					s1.pop();
+					s1.push(number);
+				}
+			}
+		}
+	}
+	//after doing this procedure , we get the top element , and that element would be our min element 
+	cout<<"THE MIN ELEMENT FOR THE STACK IS "<<s1.top()<<endl;
+}
