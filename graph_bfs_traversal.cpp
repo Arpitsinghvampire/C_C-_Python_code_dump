11cbe913ee0aacf43223db49ed5cfc777aada788
ARPIT SINGH <112745019+Arpitsinghvampire@users.noreply.github.com>
2026-07-19 23:01:48 +0530

    Add files via upload

---
+++ graph_bfs_traversal.cpp
@@ -0,0 +1,50 @@
+//graph_bfs_traversal.cpp
+
+//in this code base lets try the breadth first search traversal 
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+void breadth_first_graph(vector<vector<int>> adj , int starting_node) //here adj is the adjacency list 
+{
+	map<int , bool> visited ;
+
+	visited[starting_node] = true;
+	//now we explore all the other elements in the queue
+	queue<int> yet_to_be_visited ;
+	yet_to_be_visited.push(starting_node);
+
+
+	vector<int> breadth_first_search ;
+	while(!yet_to_be_visited.empty())
+	{
+		int front_node = yet_to_be_visited.front();
+
+		yet_to_be_visited.pop();
+
+		breadth_first_search.push_back(front_node);
+
+		for(int index = 0 ; index < adj[front_node].size() ; index++)
+
+		{
+			if(!visited(adj[front_node][index]))
+				yet_to_be_visited.push(adj[front_node][index]);
+		}
+	}
+
+	//then lets print the breadth first graph traversal 
+	for(int x : breadth_first_search)
+	{
+		cout<<x<<endl;
+	}
+
+
+
+}
+
+int main()
+{
+
+}
